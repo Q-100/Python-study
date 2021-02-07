4dd208c324853ecd2564410d7d52491c0b2f0105
Q-100 <opea5954@gmail.com>
2021-02-07 18:10:30 +0900

    반복문

---
+++ 반복문.py
@@ -0,0 +1,17 @@
+for i in range(5):  # 0부터 4까지
+    print(i)
+
+for i in range(3, 5):  # 3부터 4까지
+    print(i)
+
+# for i in list: 리스트의 원소를 모두 방문
+
+# for idx, num in enumerate(list): list의 인덱스와 값을 튜플 형태로 반환
+
+i = 0
+
+while i < 3:
+    print(i)
+    i += 1
+
+
