ec01885c954d384d61b4b2f02d38a8e1ff184326
Q-100 <opea5954@gmail.com>
2021-03-05 23:23:12 +0900

    14499

---
+++ 문제 풀이/백준_14499.py
@@ -0,0 +1,73 @@
+dice = [0, 0, 0, 0, 0, 0]
+loc = []
+N, M, x, y, K = map(int, input().split())
+for _ in range(N):
+    a = [int(z) for z in input().split()]
+    loc.append(a)
+
+command = map(int, input().split())
+
+for dir in command:
+    if dir == 1:
+        if y + 1 == M:
+            continue
+        y += 1
+        tmp = dice[0]
+        dice[0] = dice[2]
+        dice[2] = dice[5]
+        dice[5] = dice[3]
+        dice[3] = tmp
+        if loc[x][y] == 0:
+            loc[x][y] = dice[5]
+        else:
+            dice[5] = loc[x][y]
+            loc[x][y] = 0
+        print(dice[0])
+
+    elif dir == 2:
+        if y - 1 < 0:
+            continue
+        y -= 1
+        tmp = dice[0]
+        dice[0] = dice[3]
+        dice[3] = dice[5]
+        dice[5] = dice[2]
+        dice[2] = tmp
+        if loc[x][y] == 0:
+            loc[x][y] = dice[5]
+        else:
+            dice[5] = loc[x][y]
+            loc[x][y] = 0
+        print(dice[0])
+
+    elif dir == 3:
+        if x - 1 < 0:
+            continue
+        x -= 1
+        tmp = dice[0]
+        dice[0] = dice[4]
+        dice[4] = dice[5]
+        dice[5] = dice[1]
+        dice[1] = tmp
+        if loc[x][y] == 0:
+            loc[x][y] = dice[5]
+        else:
+            dice[5] = loc[x][y]
+            loc[x][y] = 0
+        print(dice[0])
+
+    elif dir == 4:
+        if x + 1 == N:
+            continue
+        x += 1
+        tmp = dice[0]
+        dice[0] = dice[1]
+        dice[1] = dice[5]
+        dice[5] = dice[4]
+        dice[4] = tmp
+        if loc[x][y] == 0:
+            loc[x][y] = dice[5]
+        else:
+            dice[5] = loc[x][y]
+            loc[x][y] = 0
+        print(dice[0])
