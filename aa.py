c6ad8ef0bfcf3690971c6206ef1fdefad89d690f
Q-100 <opea5954@gmail.com>
2021-07-14 01:50:11 +0900

    21.07.14

--- aa.py
+++ aa.py
@@ -1,35 +1,5 @@
-s = []
-for i in range(10):
-    s.append(list(map(int, input().split())))
-x, y = 1, 1
-check = 0
-if s[x][y] == 2:
-    check = 1
-s[x][y] = 9
-
-while check == 0:
-    if s[x][y + 1] == 0:
-        y += 1
-        s[x][y] = 9
-
-    elif s[x][y + 1] == 1:
-        if s[x + 1][y] == 0:
-            x += 1
-            s[x][y] = 9
-        elif s[x + 1][y] == 1:
-            s[x][y] = 9
-            break
-        else:
-            s[x + 1][y] = 9
-            break
-    else:
-        s[x][y + 1] = 9
-        break
-
-for i in range(len(s)):
-    for j in range(len(s[i])):
-        print(s[i][j], end=" ")
-    print(" ")
-
-for j in range(10):
-    print(s, end=' ')
+a,b = input().split()
+c = int(b)
+print(c, type(c))
+for i in range(c):
+    print(a,end="")
