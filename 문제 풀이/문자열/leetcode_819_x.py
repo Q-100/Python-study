223095b32948c05368cfd3036ca1a457f617c7fd
Q-100 <opea5954@gmail.com>
2021-08-04 18:15:00 +0900

    21.08.04 정규 표현식

--- 문제 풀이/문자열/leetcode_819_x.py
+++ 문제 풀이/문자열/leetcode_819_x.py
@@ -5,12 +5,13 @@ import collections
 
 
 def mostCommonWord(paragraph, banned):
-    words = [word for word in re.sub(r"[\W]"," ",paragraph).lower().split() if word not in banned]
+    words = [word for word in re.sub(r"[\W]", " ", paragraph).lower().split() if word not in banned]
     # \W로 문자나 숫자가 아닌 내용을 모두 공백으로 바꾸고 소문자로 변환 뒤 스플릿함 -> 스플릿한 단어가 banned에 없을 경우 word 리스트에 저장
     answer = collections.Counter(words)
     # Counter 객체 사용
     return answer.most_common(1)[0][0]
     # Counter 객체의 most_common을 사용
     # 가장 많은 갯수를 가진 객체를 반환한 후 0번째에 있는 단어 출력(most_common은 (ball,2)처럼 반환되기 때문에)
+
 
 print(mostCommonWord(paragraph, banned))
